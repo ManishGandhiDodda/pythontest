43a95536ba2107537b5b0ab695d7a2e57d59d799
Manish Dodda <manish.dodda@infor.com>
2019-11-20 14:54:56 +0530

    added fullstop to pythonlambda file

--- lamdafunction.py
+++ lamdafunction.py
@@ -3,7 +3,7 @@ import requests
 def call_pylenin(event=None, context=None):
     r = requests.get("https://google.com")
     if r.status_code == 200:
-        return "It was successful"
+        return "It was successful."
 
 print(call_pylenin())
 

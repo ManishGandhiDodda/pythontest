72cdac318c60555f2fda01fa7922994e2beab9cc
Manish Dodda <manish.dodda@infor.com>
2019-11-20 14:51:53 +0530

    Creating lambda fucntion to call a https URL

---
+++ lamdafunction.py
@@ -0,0 +1,9 @@
+import requests
+
+def call_pylenin(event=None, context=None):
+    r = requests.get("https://google.com")
+    if r.status_code == 200:
+        return "It was successful"
+
+print(call_pylenin())
+
